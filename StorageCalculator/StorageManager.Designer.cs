4c293a001ffe04044283c88b32f0607e285e66c3
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-01-11 18:26:06 -0500

    Adds pie chart control

--- StorageCalculator/StorageManager.Designer.cs
+++ StorageCalculator/StorageManager.Designer.cs
@@ -32,8 +32,14 @@
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cajaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editarBodegaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.DGVcajas = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlPie = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGVcajas)).BeginInit();
             this.SuspendLayout();
@@ -46,14 +52,15 @@
             this.editarBodegaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(380, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(704, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // archivoToolStripMenuItem
             // 
             this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.nuevoToolStripMenuItem});
+            this.nuevoToolStripMenuItem,
+            this.configuraciónToolStripMenuItem});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
             this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.archivoToolStripMenuItem.Text = "Archivo";
@@ -63,15 +70,21 @@
             this.nuevoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cajaToolStripMenuItem});
             this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
-            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
             this.nuevoToolStripMenuItem.Text = "Nuevo";
             // 
             // cajaToolStripMenuItem
             // 
             this.cajaToolStripMenuItem.Name = "cajaToolStripMenuItem";
-            this.cajaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cajaToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
             this.cajaToolStripMenuItem.Text = "Caja";
             this.cajaToolStripMenuItem.Click += new System.EventHandler(this.cajaToolStripMenuItem_Click);
+            // 
+            // configuraciónToolStripMenuItem
+            // 
+            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
+            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.configuraciónToolStripMenuItem.Text = "Configuración";
             // 
             // editarBodegaToolStripMenuItem
             // 
@@ -84,18 +97,58 @@
             this.DGVcajas.AllowUserToAddRows = false;
             this.DGVcajas.AllowUserToDeleteRows = false;
             this.DGVcajas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVcajas.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DGVcajas.Location = new System.Drawing.Point(0, 24);
+            this.DGVcajas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.DGVcajas.Location = new System.Drawing.Point(0, 56);
             this.DGVcajas.Name = "DGVcajas";
             this.DGVcajas.ReadOnly = true;
-            this.DGVcajas.Size = new System.Drawing.Size(380, 329);
+            this.DGVcajas.Size = new System.Drawing.Size(344, 337);
             this.DGVcajas.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LISTA DE CAJAS";
+            // 
+            // pnlPie
+            // 
+            this.pnlPie.Location = new System.Drawing.Point(350, 56);
+            this.pnlPie.Name = "pnlPie";
+            this.pnlPie.Size = new System.Drawing.Size(342, 337);
+            this.pnlPie.TabIndex = 3;
             // 
             // StorageManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(380, 353);
+            this.ClientSize = new System.Drawing.Size(704, 396);
+            this.Controls.Add(this.pnlPie);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.DGVcajas);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -118,5 +171,11 @@
         private System.Windows.Forms.ToolStripMenuItem cajaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editarBodegaToolStripMenuItem;
         private System.Windows.Forms.DataGridView DGVcajas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.ToolStripMenuItem configuraciónToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlPie;
     }
 }
--- StorageCalculator/StorageManager.cs
+++ StorageCalculator/StorageManager.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace StorageCalculator
 {
@@ -15,12 +16,15 @@ namespace StorageCalculator
     {
 
         private Storage storage;
-        private DataTable dt;
+        private DataTable table;
         private string DBconnection;
+
+        Chart pieChart;
 
         public StorageManager(Storage s)
         {
             InitializeComponent();
+            initChart();
 
             storage = s;
 
@@ -32,9 +36,9 @@ namespace StorageCalculator
         {
             using (SqlConnection conn = new SqlConnection(DBconnection))
             {
-                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Box", conn);
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT label AS Rótulo, linearMeter AS [Metros lineales ocupados], type AS Tipo FROM Box", conn);
                 SqlCommandBuilder cmdbuilder = new SqlCommandBuilder(adapter);
-                DataTable table = new DataTable();
+                table = new DataTable();
                 table.Locale = System.Globalization.CultureInfo.InvariantCulture;
                 adapter.Fill(table);
                 BindingSource bs = new BindingSource();
@@ -48,13 +52,109 @@ namespace StorageCalculator
 
         private void StorageManager_Load(object sender, EventArgs e)
         {
-           
+            updateBoxes();
+            loadpieChart();
+            //LoadBarChart();
         }
 
         private void cajaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             CreateBox cb = new CreateBox(this.Text);
-            cb.Show();
+            cb.ShowDialog();
+            updateBoxes();
         }
+
+        private void initChart()
+        {
+            this.components = new System.ComponentModel.Container();
+            ChartArea chartArea1 = new ChartArea();
+            Legend legend1 = new Legend() { BackColor = Color.Green, ForeColor = Color.Black, Title = "Salary" };
+            Legend legend2 = new Legend() { BackColor = Color.Green, ForeColor = Color.Black, Title = "Salary" };
+            pieChart = new Chart();
+
+            ((ISupportInitialize)(pieChart)).BeginInit();
+
+            SuspendLayout();
+
+            //===Pie chart
+            chartArea1.Name = "PieChartArea";
+            pieChart.ChartAreas.Add(chartArea1);
+            pieChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            pieChart.Legends.Add(legend1);
+            pieChart.Location = new System.Drawing.Point(0, 50);
+
+            //====Bar Chart
+            chartArea1 = new ChartArea();
+            chartArea1.Name = "BarChartArea";
+            legend2.Name = "Legend3";
+
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //this.ClientSize = new System.Drawing.Size(284, 262);           
+            ((ISupportInitialize)(this.pieChart)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private void loadpieChart()
+        {
+            pieChart.Series.Clear();
+            pieChart.Palette = ChartColorPalette.Fire;
+            pieChart.BackColor = Color.LightYellow;
+            pieChart.Titles.Add("Employee Salary");
+            pieChart.ChartAreas[0].BackColor = Color.Transparent;
+            Series series1 = new Series
+            {
+                Name = "series1",
+                IsVisibleInLegend = true,
+                Color = System.Drawing.Color.Green,
+                ChartType = SeriesChartType.Pie
+            };
+            pieChart.Series.Add(series1);
+            //series.Points.Add controla el tamaño de la tajada, deben ser tamaños complementarios
+            series1.Points.Add(storage.LinearMeterCapacity);
+            series1.Points.Add(0.3);
+            var p1 = series1.Points[0];
+            p1.AxisLabel = "70000";
+            p1.LegendText = "Hiren Khirsaria";
+            var p2 = series1.Points[1];
+            p2.AxisLabel = "30000";
+            p2.LegendText = "ABC XYZ";
+            pieChart.Invalidate();
+            pnlPie.Controls.Add(pieChart);
+        }
+
+        /*void LoadBarChart()
+        {
+            barChart.Series.Clear();
+            barChart.BackColor = Color.LightYellow;
+            barChart.Palette = ChartColorPalette.Fire;
+            barChart.ChartAreas[0].BackColor = Color.Transparent;
+            barChart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            barChart.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            Series series = new Series
+            {
+                Name = "series2",
+                IsVisibleInLegend = false,
+                ChartType = SeriesChartType.Column
+            };
+            barChart.Series.Add(series);
+            series.Points.Add(70000);
+            var p1 = series.Points[0];
+            p1.Color = Color.Red;
+            p1.AxisLabel = "Hiren Khirsaria";
+            p1.LegendText = "Hiren Khirsaria";
+            p1.Label = "70000";
+
+            series.Points.Add(30000);
+            var p2 = series.Points[1];
+            p2.Color = Color.Yellow;
+            p2.AxisLabel = "ABC XYZ";
+            p2.LegendText = "ABC XYZ";
+            p2.Label = "30000";
+            barChart.Invalidate();
+
+            pnlBar.Controls.Add(barChart);
+        }*/
     }
 }
